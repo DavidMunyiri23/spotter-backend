9bf646897d79c1e2e04221ab19b8349f9b41d06c
David Munyiri <davidmunyiri2019@outlook.com>
2025-10-30 23:06:36 +0300

    made a small change in the setting file

--- hos_api/settings.py
+++ hos_api/settings.py
@@ -169,6 +169,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "https://spotter-frontend-j3b9.vercel.app/"
 ]
 
 # Add production origins when deployed
